a03e4a77185f55dc077b270a55267527a025ff21
RICHARD LENIN ROJAS ROMAN <p01907f@ms.upla.edu.pe>
2024-02-13 11:30:51 -0500

    Agregar archivos de proyecto.

---
+++ ParcialP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParcialP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
